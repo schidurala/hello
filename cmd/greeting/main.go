2b5e1ae58fca1fafaa26658f412b5d2a545646f6
Sridhar Chidurala <schidurala@sp.com>
2020-09-07 12:07:01 -0400

    Initial commit

---
+++ cmd/greeting/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/schidurala/hello/morestrings"
+)
+
+func main() {
+	fmt.Println("Saying Greeting")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module cardigan.com/user/hello
+
+go 1.15
